f75c64f80593989340bceb9c282aa3fc5f0e4dcf
jlango0 <105533393+jlango-git@users.noreply.github.com>
2025-11-22 14:28:00 -0500

    Fix type error, use str.split() instead of slicing

--- tsc.py
+++ tsc.py
@@ -1,7 +1,6 @@
 orig_timestamp = input('\nWhat timestamp do you want to convert to seconds?\nUse the format 5.17, 1.30, etc. No hours for now--you can only use one period. > ')
-orig_seconds = int(orig_timestamp)[:-2]
-period_index = orig_timestamp.find('.')
-orig_minutes = int(orig_timestamp)[:period_index]
+orig_seconds = int(orig_timestamp.split('.')[1])
+orig_minutes = int(orig_timestamp.split('.')[0])
 orig_min_as_sec = orig_minutes * 60
 total_seconds = orig_min_as_sec + orig_seconds
 print(f'\nFor a YouTube timestamp, use:\t{total_seconds}\n')
